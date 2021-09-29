993e980b0eeba36003aaacacf9f30ace58ce27a5
Nata2401 <kuzyan.rd@yandex.ru>
2021-09-29 09:30:10 +0300

    Fixed: media

--- js/main.js
+++ js/main.js
@@ -23,10 +23,6 @@ const reviewsSlider = new  Swiper('.reviews-slider', {
     nextEl: '.reviews-slider__button--next',
     prevEl: '.reviews-slider__button--prev',
   },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
 });
 
 ymaps.ready(init);
